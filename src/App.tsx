af31276af4ce2e1a467573059967d3e5b10217d5
leticea <nascimento.leticeah@gmail.com>
2023-03-17 15:57:56 -0300

    feat(radix-ui): inserting toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export function App() {
       </ReactFlow>
 
       <Toolbar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
-        <Toolbar.Button></Toolbar.Button>
+        <Toolbar.Button className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2" />
       </Toolbar.Root>
     </div>
   );
