badccf8481e56934836bcdf386e75ddf847a4451
leticea <nascimento.leticeah@gmail.com>
2023-03-17 11:51:55 -0300

    feat(tailwind): inserting tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ReactFlow, { Background, Controls } from "reactflow";
+import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 
 export function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div className="w-screen h-screen">
       <ReactFlow>
-        <Background />
+        <Background gap={12} size={2} color={zinc[200]} />
       </ReactFlow>
     </div>
   );
