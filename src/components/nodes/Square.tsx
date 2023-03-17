8baef018a5184df40d81d77e9c2bf3dc9d3f1181
leticea <nascimento.leticeah@gmail.com>
2023-03-17 16:35:32 -0300

    feat(): inserting node-resizer

--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -3,12 +3,13 @@ import { NodeProps, Handle, Position } from "reactflow";
 
 import "@reactflow/node-resizer/dist/style.css";
 
-export function Square(props: NodeProps) {
+export function Square({ selected }: NodeProps) {
   return (
     <div className="bg-violet-500 rounded w-full h-full min-w-[200px] min-h-[200px]">
       <NodeResizer
         minWidth={200}
         minHeight={200}
+        isVisible={selected}
         lineClassName="border-blue-400"
         handleClassName="h-3 w-3 bg-white border-2 rounded border-blue-400"
       />
