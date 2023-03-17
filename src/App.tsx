71629ec57a560a346457fe819114d188ce90567a
leticea <nascimento.leticeah@gmail.com>
2023-03-17 12:16:35 -0300

    feat(node): inserting node_types to show the first square

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import ReactFlow, { Background, Controls } from "reactflow";
+import ReactFlow, { Background, Controls, Node } from "reactflow";
 import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
+
+import { Square } from "./components/nodes/Square";
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[];
 
 export function App() {
   return (
     <div className="w-screen h-screen">
-      <ReactFlow>
+      <ReactFlow nodeTypes={NODE_TYPES} nodes={INITIAL_NODES}>
         <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
       </ReactFlow>
     </div>
   );
