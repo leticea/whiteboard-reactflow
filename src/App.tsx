565e23647f40b0081ded6ec6b2f99f465ca9d590
leticea <nascimento.leticeah@gmail.com>
2023-03-17 12:36:55 -0300

    feat(useEdgesState): inserting useEdgesState to store the connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import ReactFlow, { Background, Controls, Node } from "reactflow";
+import ReactFlow, {
+  Background,
+  Connection,
+  ConnectionMode,
+  Controls,
+  Node,
+  useEdgesState,
+} from "reactflow";
 import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 
 import { Square } from "./components/nodes/Square";
+import { useCallback } from "react";
 
 const NODE_TYPES = {
   square: Square,
@@ -18,12 +26,31 @@ const INITIAL_NODES = [
     },
     data: {},
   },
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
 ] satisfies Node[];
 
 export function App() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => )
+  }, [])
+
   return (
     <div className="w-screen h-screen">
-      <ReactFlow nodeTypes={NODE_TYPES} nodes={INITIAL_NODES}>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={INITIAL_NODES}
+        connectionMode={ConnectionMode.Loose}
+      >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
